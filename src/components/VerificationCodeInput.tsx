504f1c48b9d0f703a59094eb5c01237c709dfd11
cnnrmnn <connor.mann@outlook.com>
2021-03-23 17:10:57 -0400

    Add basic verification code input

---
+++ src/components/VerificationCodeInput.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  value: string;
+  setValue(value: string): void;
+};
+
+export default function VerificationCodeInput({
+  value,
+  setValue,
+}: Props): JSX.Element {
+  function handleChange(event: React.FormEvent<HTMLInputElement>): void {
+    if (/^[0-9]*$/.test(event.currentTarget.value))
+      setValue(event.currentTarget.value);
+  }
+  return (
+    <input type="text" maxLength={6} value={value} onChange={handleChange} />
+  );
+}
