c472383ddadeb9cd09c73cc24ec2a655169753ae
cnnrmnn <connor.mann@outlook.com>
2021-03-27 10:59:41 -0400

    Add image and description to item box

--- src/components/ItemBox.tsx
+++ src/components/ItemBox.tsx
@@ -1,5 +1,5 @@
-import './ItemBox.css';
 import { ItemValue } from '../api/value';
+import './ItemBox.css';
 
 type Props = {
   itemValue: ItemValue;
@@ -8,7 +8,17 @@ type Props = {
 export default function ItemBox({ itemValue }: Props): JSX.Element {
   return (
     <div className="item-box">
-      <h4>{itemValue.value}</h4>
+      <img
+        className="item-box-image"
+        src="https://i.ibb.co/7WrZ8n5/97fd828addb04f04e8e6b374214f4d8b.png"
+        alt={itemValue.value}
+      />
+      <div className="item-box-text">
+        <h5 className="item-box-heading">{itemValue.value}</h5>
+        <p className="item-box-description">
+          Fried onion petals with a crispy, flaky breading.
+        </p>
+      </div>
     </div>
   );
 }
