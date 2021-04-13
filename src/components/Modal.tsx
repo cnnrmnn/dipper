4851c8fd7094cc4cc581132e6cdebc033ce1b526
cnnrmnn <connor.mann@outlook.com>
2021-04-13 16:32:33 -0400

    Add additional methods to close modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,17 +17,18 @@ export default function Modal({
   setVisible,
   children,
 }: Props): JSX.Element {
-  const node = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   function setSiblingFilters(value: string): void {
-    // Use optional chaining to keep code concise. If node.current is null,
+    // Use optional chaining to keep code concise. If containerRef.current is null,
     // parent will be undefined and the code will work as intended.
-    const parent = node.current?.parentElement;
+    const parent = containerRef.current?.parentElement;
     if (parent) {
       let sibling = parent.firstChild as HTMLElement | null;
       while (sibling) {
         if (
-          sibling !== node.current &&
+          sibling !== containerRef.current &&
           sibling.nodeType === Node.ELEMENT_NODE
         ) {
           sibling.style.filter = value;
@@ -52,9 +53,26 @@ export default function Modal({
     setSiblingFilters('');
     setVisible(false);
   }
+
+  function handleClick(event: React.MouseEvent): void {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      closeModal();
+    }
+  }
+
+  function handleKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown, false);
+    return () => document.removeEventListener('keydown', handleKeyDown, false);
+  });
   return (
-    <div className={container} ref={node}>
-      <div className={modal} style={{ height, width }}>
+    <div className={container} onClick={handleClick} ref={containerRef}>
+      <div className={modal} ref={modalRef} style={{ height, width }}>
         <div className={header}>
           <h2 className={heading}>{title}</h2>
           <ModalButton closeModal={closeModal} />
