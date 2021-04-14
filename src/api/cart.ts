7104485576af2b836695869331b39e05dd5db753
cnnrmnn <connor.mann@outlook.com>
2021-04-14 16:32:59 -0400

    Add item and extra to triple dipper

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -6,9 +6,20 @@ export interface ItemInput {
   extras: number[];
 }
 
+export interface Extra {
+  valueId: number;
+  value: string;
+}
+
+export interface Item {
+  valueId: number;
+  value: string;
+  extras: Extra[];
+}
 export interface TripleDipper {
   id: number;
   orderId: number;
+  items: Item[];
 }
 
 export async function addToCart(items: ItemInput[]): Promise<TripleDipper> {
@@ -17,6 +28,14 @@ export async function addToCart(items: ItemInput[]): Promise<TripleDipper> {
       addToCart(items: $items) {
         id
         orderId
+        items {
+          valueId
+          value
+          extras {
+            valueId
+            value
+          }
+        }
       }
     }
   `;
