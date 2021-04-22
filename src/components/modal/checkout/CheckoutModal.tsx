c15422821f70a12e81868192c096075aee13b518
cnnrmnn <connor.mann@outlook.com>
2021-04-22 11:56:49 -0400

    Add item overflow scrolling

--- src/components/modal/checkout/CheckoutModal.tsx
+++ src/components/modal/checkout/CheckoutModal.tsx
@@ -2,9 +2,9 @@ import { Order } from '../../../api/order';
 import Button from '../../generic/Button';
 import Modal from '../Modal';
 import CheckoutModalItem from './CheckoutModalItem';
-import { heading, button } from './CheckoutModal.css';
 import CheckoutModalAddress from './CheckoutModalAddress';
 import CheckoutModalReceipt from './CheckoutModalReceipt';
+import { heading, button, top, bottom, items } from './CheckoutModal.css';
 
 type Props = {
   order: Order | null;
@@ -13,21 +13,33 @@ type Props = {
 
 export default function CheckoutModal({ order, close }: Props): JSX.Element {
   return (
-    <Modal title="Checkout" height="auto" width="350px" close={close}>
+    <Modal
+      title="Checkout"
+      maxHeight="80%"
+      minHeight="400px"
+      width="350px"
+      close={close}
+    >
       {order ? (
         <>
-          <h3 className={heading}>Deliver to</h3>
-          <CheckoutModalAddress address={order.address} />
-          <h3 className={heading}>Items</h3>
-          {order.tripleDippers.map((tripleDipper) => (
-            <CheckoutModalItem
-              key={tripleDipper.id}
-              tripleDipper={tripleDipper}
-            />
-          ))}
-          <CheckoutModalReceipt order={order} />
-          <div className={button}>
-            <Button text="Place order" fontSize="1rem" />
+          <div className={top}>
+            <h3 className={heading}>Deliver to</h3>
+            <CheckoutModalAddress address={order.address} />
+            <h3 className={heading}>Items</h3>
+          </div>
+          <div className={items}>
+            {order.tripleDippers.map((tripleDipper) => (
+              <CheckoutModalItem
+                key={tripleDipper.id}
+                tripleDipper={tripleDipper}
+              />
+            ))}
+          </div>
+          <div className={bottom}>
+            <CheckoutModalReceipt order={order} />
+            <div className={button}>
+              <Button text="Place order" fontSize="1rem" />
+            </div>
           </div>
         </>
       ) : (
