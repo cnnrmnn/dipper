79ebd905a22c86f45730e53e913eedd65d8f13c5
cnnrmnn <connor.mann@outlook.com>
2021-04-25 18:12:07 -0400

    Adjust modal form margins and children

--- src/components/modal/ModalForm.tsx
+++ src/components/modal/ModalForm.tsx
@@ -2,7 +2,7 @@ import styles from './ModalForm.css';
 
 type Props = {
   onSubmit(event: React.SyntheticEvent): Promise<void>;
-  children: (string | JSX.Element)[];
+  children: (string | JSX.Element)[] | JSX.Element;
 };
 
 export default function ModalForm({ onSubmit, children }: Props): JSX.Element {
